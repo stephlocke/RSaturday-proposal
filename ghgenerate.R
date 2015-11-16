54ddcb513edde7ff63d3fe42bde4870c3e2a64f6
Steph Locke <slocke@mango-solutions.com>
2015-11-16 21:06:23 +0000

    Added the barebones of an organiser guide Uses feedback received so far - not final in anyway!

--- ghgenerate.R
+++ ghgenerate.R
@@ -1,8 +1,13 @@
-proposal<-"RSaturdays [WIP]"
+proposal<-"satRdays [WIP]"
 proposal.file<-"isc-proposal.Rmd"
 author<-"Steph Locke"
 
+# Proposal
 rmarkdown::render(proposal.file, output_format="html_document",
                   output_dir="out", quiet=TRUE)
 rmarkdown::render(proposal.file, output_format="pdf_document",
+                  output_dir="out", quiet=TRUE)
+
+# Additional docs
+rmarkdown::render("satRday_organiser.Rmd", output_format="html_document",
                   output_dir="out", quiet=TRUE)
